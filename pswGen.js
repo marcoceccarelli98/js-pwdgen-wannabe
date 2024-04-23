0aa267a4ade88a71bc3d012dcddc3b960c472946
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-04-23 19:06:33 +0200

    [UPD] Added script for psw generator

--- pswGen.js
+++ pswGen.js
@@ -1,2 +1,19 @@
 'use strict'
 
+let element = document.getElementById('psw');
+let text = element.innerHTML;
+
+// INPUT NAME
+let userName = prompt('Inserisci il nome');
+// INPUT SURNAME
+let userLastName = prompt('Inserisci il cognome');
+// INPUT COLOR
+let color = prompt('Inserisci il tuo colore preferito');
+// RANDOM NUMBER
+let number = Math.floor(Math.random() * 101);
+
+text = `${userName}${userLastName}${color}${number}`;
+
+console.log(text);
+
+element.innerHTML = text;
